b0cb27628adb86ce16feed408b838fc117d9db91
Jesus Chero <cheroarge96@gmail.com>
2022-06-25 22:44:54 -0500

    usando boolean

---
+++ src/ClasesJavaSE/Boolean.java
@@ -0,0 +1,23 @@
+package ClasesJavaSE;
+
+public class Boolean {
+	public static void main(String[] args) {
+		
+		/*usando boolean*/
+		
+		int n1 = 2;
+		float n2 =2;
+		System.out.println("numero tipo entero : "+n1);
+		System.out.println("numero tipo flotante : "+n2);
+		
+		boolean respuesta = true;
+		System.out.println(respuesta);
+		
+		respuesta = n1 > n2;
+		System.out.println(respuesta);
+		
+		
+		
+	}
+
+}
