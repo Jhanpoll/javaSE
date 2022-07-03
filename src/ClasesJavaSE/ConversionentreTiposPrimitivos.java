02de10a2be2b5aeac3eb7bc9c543eed66862d5fa
Jesus Chero <cheroarge96@gmail.com>
2022-07-03 01:09:01 -0500

    realizando  src/ClasesJavaSE/ConversionentreTiposPrimitivos.java

---
+++ src/ClasesJavaSE/ConversionentreTiposPrimitivos.java
@@ -0,0 +1,20 @@
+package ClasesJavaSE;
+
+public class ConversionentreTiposPrimitivos {
+	
+	public static void main(String[] args) {
+		int numeroEntero = 123456;
+		
+		
+		long numeroLong = numeroEntero;
+		System.out.println("de int a long no hay que forzar casteo : "+ numeroLong);
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Double.MAX_VALUE);
+		
+		short numeroShort = (short)numeroEntero;
+		System.out.println("de int a short si es necesario el forzar casteo: "+numeroShort);
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Short.MAX_VALUE);
+	}
+
+}
