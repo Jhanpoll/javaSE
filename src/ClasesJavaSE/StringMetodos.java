96059f3d6cf5d5af0b880e8eebdfb97ad874fe3d
Jesus Chero <cheroarge96@gmail.com>
2022-07-04 20:00:13 -0500

    usando los metodos de string en src/ClasesJavaSE/StringMetodos.java

---
+++ src/ClasesJavaSE/StringMetodos.java
@@ -0,0 +1,31 @@
+package ClasesJavaSE;
+
+public class StringMetodos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nombre = "jesus jhanpoll chero arge";
+		String curso = "    programacion";
+		String trabalenguas = "trabalenguas";
+		System.out.println(nombre.toUpperCase());
+		System.out.println(nombre.charAt(0));
+		System.out.println(nombre.charAt(1));
+		System.out.println(nombre.equals("jesus"));
+		System.out.println(nombre.length());
+		System.out.println(nombre.substring(2));
+		System.out.println(nombre.substring(2,4));
+		System.out.println(nombre.substring(nombre.length()-1));
+		
+		System.out.println(curso);
+		System.out.println(curso.trim());
+		System.out.println(trabalenguas.replace("a", "A"));
+		System.out.println(trabalenguas.endsWith("lenguas"));
+		System.out.println(trabalenguas.startsWith("tra"));
+		System.out.println(trabalenguas.indexOf("a"));
+		System.out.println(trabalenguas.lastIndexOf("a"));
+		
+		
+
+	}
+
+}
