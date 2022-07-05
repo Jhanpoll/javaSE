6ddc2031a718c59732c83f6a325103e0f59517b8
Jesus Chero <cheroarge96@gmail.com>
2022-07-04 23:15:25 -0500

    utilizando los metodos de la clase string para obtener la extension de cualquier archivo

---
+++ src/ClasesJavaSE/EjemplodeExtensionDeArchivos.java
@@ -0,0 +1,17 @@
+package ClasesJavaSE;
+
+import java.util.Scanner;
+
+public class EjemplodeExtensionDeArchivos {
+	public static void main(String[] args) {
+		
+		//String archivo ="imagen.jesus.pdf";
+		System.out.print("ingrese el nombre del archivo con su extension: ");
+		Scanner sc = new Scanner(System.in);
+		String archivo = sc.nextLine();
+		/*es mejor el lastIndexOf debido a que puede existir varios puntos 
+		 * y lo que queremos es tomar el ultimo punto*/
+		int ultimoPunto = archivo.lastIndexOf(".");
+		System.out.println("la extension del archivo es: "+archivo.substring(ultimoPunto));
+	}
+}
