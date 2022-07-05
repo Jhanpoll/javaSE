550f757e38e9b23183587fed6f23231e58fcdb9f
Jesus Chero <cheroarge96@gmail.com>
2022-07-04 19:59:42 -0500

    usando src/ClasesJavaSE/ValidarUnString.java

---
+++ src/ClasesJavaSE/ValidarUnString.java
@@ -0,0 +1,27 @@
+package ClasesJavaSE;
+
+public class ValidarUnString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String nombre = null;
+		String apellido = "chero";
+		
+		System.out.println("imprimiendo nombre : "+nombre);
+		//System.out.println("concanternar nombre y apellido: "+nombre+apellido);
+		boolean esNulo = true;
+		boolean esVacio= true;
+		
+		if(!esNulo) {
+			nombre = " ";
+			System.out.println(nombre+apellido);
+		}
+		
+		System.out.println(esNulo);
+		System.out.println(esVacio);
+
+
+	}
+
+}
